6f0bbedf70a836bb0805d068939d9e5d969c3a69
ryuuichi1457 <ryuhashi1457@gmail.com>
2023-05-15 21:55:00 +0900

    垂れ流し

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-
+    print(message.content)
     today = datetime.date.today().day
     response=None
     if  message.content == '時間割':
